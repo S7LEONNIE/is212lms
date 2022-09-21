e76fff5efff2826269c590a63f52ef532f557eb8
Daniel Chng <danielchng10@hotmail.com>
2022-09-22 02:51:07 +0800

    CSS Reset

--- index.php
+++ index.php
@@ -1,9 +1,16 @@
 <!DOCTYPE html>
 <html>
     <head>
-
+        <title>LJMS - Home</title>
+        
+        <link rel="stylesheet" href="css/main.css">
     </head>
     <body>
-        
+
+        <?php include 'component/navbar.php'; ?>
+
+        <div class='main-content'>
+            main content here
+        </div>
     </body>
 </html>
