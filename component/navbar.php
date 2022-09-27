28ab8de1a43298aaf9821cb284b344ccf0d65591
Daniel Chng <danielchng10@hotmail.com>
2022-09-27 21:27:44 +0800

    Creation of navbar

---
+++ component/navbar.php
@@ -0,0 +1,31 @@
+<div class="Navbar__Wrapper">
+    <div class="Navbar">
+        <div class="Navbar__Link Navbar__Link-brand">
+            LJMS
+        </div>
+        <div class="Navbar__Link Navbar__Link-toggle">
+            <i class="fas fa-bars"></i>
+        </div>
+
+        <nav class="Navbar__Items">
+            <div class="Navbar__Link">
+                <a href="index.php">Home</a>
+            </div>
+            <div class="Navbar__Link">
+            <a href="job.php">Job</a>
+            </div>
+            <div class="Navbar__Link">
+            <a href="skillcourse.php">Skills/Courses</a>
+            </div>
+        </nav>
+
+        <nav class="Navbar__Items Navbar__Items--right">
+            <div class="Navbar__Link">
+                <a href="myjourney.php">My Journey</a>
+            </div>
+            <div class="Navbar__Link">
+                <a href="profile.php">Hi, xxxx</a>
+            </div>
+        </nav>
+    </div>
+</div>
