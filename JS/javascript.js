d61b257687885980e5c76a77824c6f0231b0ed86
Daniel Chng <danielchng10@hotmail.com>
2022-10-04 17:10:04 +0800

    Home Page

--- JS/javascript.js
+++ JS/javascript.js
@@ -1,5 +1,23 @@
+
+$(document).ready(function(){
+	// Run our swapImages() function every 5secs
+	setInterval('carouselChange()', 8000);
+  })
+
+
 function classToggle() {
-    const navs = document.querySelectorAll('.Navbar__Items')
-    
-    navs.forEach(nav => nav.classList.toggle('Navbar__ToggleShow'));
-  }
+	const navs = document.querySelectorAll('.Navbar__Items')
+
+	navs.forEach(nav => nav.classList.toggle('Navbar__ToggleShow'));
+}
+
+
+function carouselChange() {
+	var $active = $('.carousel .carousel-shown');
+	var $next = ($('.carousel .carousel-shown').next().length > 0) ? $('.carousel .carousel-shown').next() : $('.carousel li:first');
+	
+	$active.fadeOut(function(){
+		$active.removeClass('carousel-shown');
+		$next.fadeIn().addClass('carousel-shown');
+	});
+}
