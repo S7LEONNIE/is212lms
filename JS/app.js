15d6d9d45233c9abf26fd15cab569cc5fda1c1a8
Leon Tan <la9354@hotmail.com>
2022-10-07 00:55:49 +0800

    Linked backend to index.html

--- JS/app.js
+++ JS/app.js
@@ -2,6 +2,8 @@ const app = Vue.createApp({
     data() {
         return {
             roles: [],
+            skills: [],
+            courses: [],
         }
     },
     methods: {
@@ -16,10 +18,39 @@ const app = Vue.createApp({
                 .catch(error => {
                 console.log(error.message);
                 });
+        },
+
+        getSkills() {
+            axios.get("PHP/functionGetAllSkills.php")
+                .then(response => {
+                if (response.status == 200) {
+                    console.log(response.data)
+                    this.skills = response.data.records
+                }
+                })
+                .catch(error => {
+                console.log(error.message);
+                });
+        },
+
+        getCourses() {
+            axios.get("PHP/functionGetAllCourses.php")
+                .then(response => {
+                if (response.status == 200) {
+                    console.log(response.data)
+                    this.courses = response.data.records
+                }
+                })
+                .catch(error => {
+                console.log(error.message);
+                });
         }
+
     },
     beforeMount(){
         this.getRoles();
+        this.getSkills();
+        this.getCourses();
     },
 })
 
