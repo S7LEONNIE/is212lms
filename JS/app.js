607a98ad70af1a51a89f79842a2a908a8e91b65c
ANG Yue Jun (Joseph) <angyj.joseph@gmail.com>
2022-10-17 17:30:11 +0800

    job detail linkage
    Initial linkage of backend to frontend for View One Job.

--- JS/app.js
+++ JS/app.js
@@ -7,6 +7,13 @@ const app = Vue.createApp({
             skills: [],
             courses: [],
             skillsAndCourses: [],
+            role_details_exists: true,
+            role_details: {
+                role_id: "",
+                role_name: "",
+                role_desc: ""
+            },
+            skillsByRole: []
         }
     },
     methods: {
@@ -54,12 +61,63 @@ const app = Vue.createApp({
                 .then(response => {
                 if (response.status == 200) {
                     console.log(response.data)
-                    this.skillsAndCourses = response.data.records
+                    this.skillsAndCourses = response.data.records;
                 }
                 })
                 .catch(error => {
                 console.log(error.message);
                 });
+        },
+        
+        getRoleById() {
+            // url: 'skeleton_view_one_role?role_id=1&foo=1&bar=2');
+            let urlParams = window.location.search.substring(1).split("&");
+            let params = {};
+            for (let param of urlParams) {
+                let temp = param.split("=");
+                let key = temp[0];
+                let val = temp[1];
+                params[key] = val;
+            }
+            
+            try {
+                let role_id = params.job_id;
+
+                axios.post("PHP/functionGetRoleById.php", 
+                {
+                    role_id: role_id
+                })
+                    .then(response => {
+                    if (response.status == 200) {
+                        console.log(response.data);
+                        role_details_exists = true;
+                        this.role_details = response.data.records;
+                    }
+                    })
+                    .catch(error => {
+                    console.log(error.message);
+                    console.log(error.response.data.status);
+                    role_details_exists = false;
+                    });
+    
+                axios
+                .post("PHP/functionGetSkillsByRole.php", {
+                    roleId: role_id,
+                })
+                .then(response => {
+                    if (response.status == 200) {
+                        console.log(response.data)
+                        this.skillsByRole = response.data.records
+                    }
+                })
+                .catch(error => {
+                    console.log(error.message);
+                });
+            }
+            
+            catch {
+                role_details_exists = false
+            }
         }
 
     },
@@ -68,6 +126,7 @@ const app = Vue.createApp({
         this.getSkills();
         this.getCourses();
         this.getSkillsAndCourses();
+        this.getRoleById();
     },
 })
 
