80b9ad48d159db0465758cbd78c0c7d79e000826
LaLaNiceApp <eunjin.kim.2020@sis.smu.edu.sg>
2022-10-22 16:50:08 +0800

    Update FirstCest.php

--- tests/Acceptance/FirstCest.php
+++ tests/Acceptance/FirstCest.php
@@ -21,5 +21,36 @@ class FirstCest
         $I->amOnPage('/');
         $I->see('Home');
     }
+
+    public function loginWorks(AcceptanceTester $I)
+    {
+        $I->amOnPage('/login');
+        $I->see('Login');
+    }
+    
+    public function loginSuccesful(AcceptanceTester $I)
+    {
+        // $I->amOnPage('/login');
+        // $I->fillField('username', 'davert');
+        // $I->fillField('password', 'qwerty');
+        // $I->click('LOGIN');
+        // $I->see('Welcome, Davert!');
+    }
+
+    public function searchbarWorks(AcceptanceTester $I)
+    {
+        $I->amOnPage('/');
+        $I->see('Search');
+    }
+
+    public function searchbarResults(AcceptanceTester $I)
+    {
+        $I->amOnPage('/');
+        $I->fillField('Search', 'Defense');
+        $I->click('Search');
+        $I->see('Defense against the Dark Arts');
+    }
+
 }
 
+// php vendor/bin/codecept run --steps
