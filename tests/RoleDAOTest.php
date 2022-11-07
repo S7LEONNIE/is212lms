4d862c21539b7369a9f00aaadd7b98a01116aa81
ANG Yue Jun (Joseph) <angyj.joseph@gmail.com>
2022-11-07 13:38:11 +0800

    Initial Integration Test

---
+++ tests/RoleDAOTest.php
@@ -0,0 +1,28 @@
+<?php
+
+// namespace Tests\Unit;
+require_once '.\PHP\classRoleDAO.php';
+
+use Tests\Support\UnitTester;
+
+class testRoleDAOTest extends \Codeception\Test\Unit
+{
+
+    protected UnitTester $tester;
+
+    protected function _before()
+    {
+    }
+
+    // tests
+    public function testAddRole()
+    {
+        $roleDAO = new classRoleDAO();
+        $status = $roleDAO->addRole("The Wandering Emperor", "This role exists.");
+        $this->tester->seeInDatabase('role', [
+            'role_name' => 'The Wandering Emperor', 
+            'role_desc' => 'This role exists.',
+            'is_active' => 'active'
+        ]);
+    }
+}
--- tests/RoleTest.php
+++ tests/RoleTest.php
@@ -1,9 +1,6 @@
 <?php
 
 // namespace Tests\Unit;
-
-// namespace PHP;
-
 require_once '.\PHP\classRole.php';
 
 use Tests\Support\UnitTester;
