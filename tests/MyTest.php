34e15a466ed2d2092293243dedda4d2a6f441245
ANG Yue Jun (Joseph) <angyj.joseph@gmail.com>
2022-11-07 13:14:06 +0800

    setup

---
+++ tests/MyTest.php
@@ -0,0 +1,31 @@
+<?php
+
+// namespace Tests\Unit;
+
+// namespace PHP;
+
+require_once '.\PHP\classRole.php';
+
+use Tests\Support\UnitTester;
+
+class MyTest extends \Codeception\Test\Unit
+{
+    protected UnitTester $tester;
+
+    protected function _before()
+    {
+    }
+
+    // tests
+    public function testSomeFeature()
+    {
+        $role = new classRole(FALSE, TRUE, FALSE, TRUE);
+        $this->assertEquals($role->getRole_Id(), FALSE);
+
+        // $user->setName('toolooooongnaaaaaaameeee');
+        // $this->assertFalse($user->validate(['username']));
+
+        // $user->setName('davert');
+        // $this->assertTrue($user->validate(['username']));
+    }
+}
